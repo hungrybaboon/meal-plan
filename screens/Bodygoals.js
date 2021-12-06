6e7f6d176787a65c532cd026527414aa51d9e563
hungrybaboon <66892728+hungrybaboon@users.noreply.github.com>
2021-12-06 11:36:22 -0500

    updated UI

--- screens/Bodygoals.js
+++ screens/Bodygoals.js
@@ -1,6 +1,6 @@
-import { Button } from 'react-native-elements';
 import React,{useState} from "react";
 import { View } from "react-native";
+import Button from '../components/Button';
 
 export default function BodyGoals({route,navigation}){
     const {tdee,lose,gain} = route.params;
@@ -11,7 +11,7 @@ export default function BodyGoals({route,navigation}){
     }
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Button title='Lose Weight' raised type="outline"onPress={ ()=>{
+            <Button style={{ margin: 5 }}label='Lose Weight' onPress={ ()=>{
                 fetch(`https://api.spoonacular.com/mealplanner/generate?timeFrame=week&apiKey=0e5f3b97a15746b4b5d2b2d5ac294240&targetCalories=${lose}`)
                 .then((response) => response.json())
                 .then((json) => {
@@ -19,7 +19,7 @@ export default function BodyGoals({route,navigation}){
                 })
                 .catch((error) => console.error(error));
             }} />
-            <Button title='Gain Weight' raised type="outline"onPress={()=>{
+            <Button style={{ margin: 5 }} label='Gain Weight' onPress={()=>{
                 fetch(`https://api.spoonacular.com/mealplanner/generate?timeFrame=week&apiKey=0e5f3b97a15746b4b5d2b2d5ac294240&targetCalories=${gain}`)
                 .then((response) => response.json())
                 .then((json) => {
@@ -27,7 +27,7 @@ export default function BodyGoals({route,navigation}){
                 })
                 .catch((error) => console.error(error));
             }} />
-            <Button title='Maintain Weight' raised type="outline"onPress={()=>{
+            <Button style={{ margin: 5 }} label='Maintain Weight' onPress={()=>{
                 fetch(`https://api.spoonacular.com/mealplanner/generate?timeFrame=week&apiKey=0e5f3b97a15746b4b5d2b2d5ac294240&targetCalories=${tdee}`)
                 .then((response) => response.json())
                 .then((json) => {
@@ -35,62 +35,7 @@ export default function BodyGoals({route,navigation}){
                 })
                 .catch((error) => console.error(error));
             }} />
-            <Button title='Arms' raised type="outline"onPress={()=>{
-                fetch(`https://wger.de/api/v2/exercise/?language=2&limit=200&muscles=1,5,13`)
-                .then((response) => response.json())
-                .then((json) => {
-                navigation.navigate('Workout',{title: 'Arms', workout:json});
-                })
-                .catch((error) => console.error(error));
-            }} />
-            <Button title='Legs' raised type="outline"onPress={()=>{
-                fetch(`https://wger.de/api/v2/exercise/?language=2&limit=200&muscles=7,8,10,11,15`)
-                .then((response) => response.json())
-                .then((json) => {
-                navigation.navigate('Workout',{title: 'Legs', workout:json});
-                })
-                .catch((error) => console.error(error));
-            }} />
-            <Button title='Chest' raised type="outline"onPress={()=>{
-                fetch(`https://wger.de/api/v2/exercise/?language=2&limit=200&muscles=4`)
-                .then((response) => response.json())
-                .then((json) => {
-                navigation.navigate('Workout',{title: 'Chest', workout:json});
-                })
-                .catch((error) => console.error(error));
-            }} />
-            <Button title='Back' raised type="outline"onPress={()=>{
-                fetch(`https://wger.de/api/v2/exercise/?language=2&limit=200&muscles=12`)
-                .then((response) => response.json())
-                .then((json) => {
-                navigation.navigate('Workout',{title: 'Back', workout:json});
-                })
-                .catch((error) => console.error(error));
-            }} />
-            <Button title='Shoulder' raised type="outline"onPress={()=>{
-                fetch(`https://wger.de/api/v2/exercise/?language=2&limit=200&muscles=2,9`)
-                .then((response) => response.json())
-                .then((json) => {
-                navigation.navigate('Workout',{title: 'Shoulder', workout:json});
-                })
-                .catch((error) => console.error(error));
-            }} />
-            <Button title='Tri/Chest/Shoulder' raised type="outline"onPress={()=>{
-                fetch(`https://wger.de/api/v2/exercise/?language=2&limit=200&muscles=2,4,5,9`)
-                .then((response) => response.json())
-                .then((json) => {
-                navigation.navigate('Workout',{title: 'Tri/Chest/Shoulder', workout:json});
-                })
-                .catch((error) => console.error(error));
-            }} />
-            <Button title='Back/Bi' raised type="outline"onPress={()=>{
-                fetch(`https://wger.de/api/v2/exercise/?language=2&limit=200&muscles=1,12,13`)
-                .then((response) => response.json())
-                .then((json) => {
-                navigation.navigate('Workout',{title: 'Back/Bi', workout:json});
-                })
-                .catch((error) => console.error(error));
-            }} />
+            
         </View>
     );
 }
